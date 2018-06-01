e3f7897127bb1e2741559f8cd6f7b35a606a9495
PedroAngeli <pedroans.angeli@hotmail.com>
2018-06-01 13:19:06 -0300

    Docente criado

---
+++ src/gerencia/atividade/Docente.java
@@ -0,0 +1,15 @@
+package gerencia.atividade;
+
+public class Docente {
+
+	String nome;
+	int codigo;
+	String departamento;
+	
+	public Docente(String nome,int codigo,String departamento)
+	{
+		this.codigo = codigo;
+		this.nome = nome;
+		this.departamento = departamento;
+	}
+}
