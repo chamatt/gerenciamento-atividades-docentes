9753a3cb4c2df8d8c06729dd2248b9937821ea1a
chamatt <matt.vicent@gmail.com>
2018-06-03 21:10:07 -0300

    Testador funcionando e acusando que está correto!
    Só falta a serializaçao.

--- src/exceptions/NivelCursoInconsistenteException.java
+++ src/exceptions/NivelCursoInconsistenteException.java
@@ -2,6 +2,6 @@ package exceptions;
 
 public class NivelCursoInconsistenteException extends Exception {
 	public NivelCursoInconsistenteException(String nome, int codigo) {
-		System.out.println("Inconsistência ao definir o nível do curso " + nome  + " - " + codigo +".");	
+		System.out.println("Inconsistência ao definir o nível do curso: " + codigo  + " - " + nome +".");	
 	}
 }
